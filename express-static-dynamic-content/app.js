d5e6affac122edb2eefb19a6cf02b1a4b72279f2
Enes-Dogan <enesdogan210@gmail.com>
2023-01-15 04:27:36 +0300

    404 and 500 error handling"

--- express-static-dynamic-content/app.js
+++ express-static-dynamic-content/app.js
@@ -43,6 +43,8 @@ app.get('/restaurants/:id', function (req, res) {
       return res.render('restaurant-detail', { restaurant: restaurant });
     }
   }
+
+  res.render('404');
 });
 
 app.get('/recommend', function (req, res) {
@@ -72,5 +74,13 @@ app.get('/confirm', function (req, res) {
 app.get('/about', function (req, res) {
   res.render('about');
 });
+
+app.use(function (req, res) {
+  res.render('404');
+});
+
+app.use(function (error, req, res, next) {
+  res.render('500'); 
+})
 
 app.listen(3000);
