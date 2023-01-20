6968b16e54d00051c71d645576e5e4b30cbdfc3f
Enes-Dogan <enesdogan210@gmail.com>
2023-01-20 23:40:29 +0300

    template literals, rest/spread operators"

--- adv-javascript/functions.js
+++ adv-javascript/functions.js
@@ -13,7 +13,7 @@ function sumUp(num1, num2, num3) {
 }
 // Limitation with this functions is we can -actually must-
 // accept 3 parameters and not very flexible
-console.log(sumUp(1, 2)); // NaN
+console.log(sumUp(1, 2)); // NaN  (undefined + integer)
 
 //Array method
 
@@ -26,4 +26,39 @@ function sumUp2(numbers) {
   return result;
 }
 
-console.log(sumUp2([1, 6, 12, 4]));
+console.log(sumUp2([1, 6, 12, 4])); // 23
+
+// Rest Parameters
+
+function sumUp3(...numbers) {
+  let result = 0;
+
+  for (const number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+console.log(sumUp3(1, 8, 15, 23)); // 47
+
+//Also can be used on array to turn it individual lists of item
+
+function sumUp4(...numbers) {
+  let result = 0;
+
+  for (const number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+const inputNumbers = [1, 6, 12, 4];
+console.log(sumUp4(...inputNumbers)); // 23
+
+// Template literals
+function greetUser2(greetingPrefix, name = 'user') {
+  console.log(`${greetingPrefix} ${name}!`);
+}
+
+greetUser('hi', 'enes');
+greetUser('hello');
