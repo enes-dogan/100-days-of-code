abf4e6fe162a348f48a45ee40ca904c465012e5e
Enes-Dogan <enesdogan210@gmail.com>
2023-01-24 17:34:05 +0300

    custom error handling

--- adv-javascript/errors.js
+++ adv-javascript/errors.js
@@ -3,10 +3,25 @@ const fs = require('fs');
 function readFile() {
   try {
     const fileData = fs.readFileSync('data.json');
-  } catch {
-    console.log('An error occurred!');
+  } catch (error) {
+    console.log('Error occurred.');
+    console.log(error.message);
   }
-  console.log('Hi there!');
+  console.log('End of function.');
 }
 
 readFile();
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+try {
+  let result = divide(5, 0);
+  console.log(result);
+} catch (e) {
+  console.log(e.message); // "Cannot divide by zero"
+}
