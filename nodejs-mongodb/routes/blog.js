ef946b659501f25dc8cdf967c9f291572d637514
Enes-Dogan <enesdogan210@gmail.com>
2023-02-08 00:24:00 +0300

    get db

--- nodejs-mongodb/routes/blog.js
+++ nodejs-mongodb/routes/blog.js
@@ -50,12 +50,20 @@ router.post('/posts', async function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts/:id', async function (req, res) {
-  const postId = req.params.id;
+router.get('/posts/:id', async function (req, res, next) {
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId)
+  } catch (error) {
+    console.log(error);
+    return res.status(404).render('404');
+    // return next(error);
+  }
   const post = await db
     .getDb()
     .collection('posts')
-    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+    .findOne({ _id: postId }, { summary: 0 });
 
   if (!post) {
     return res.status(404).render('404');
@@ -98,9 +106,20 @@ router.post('/posts/:id/edit', async function (req, res) {
           title: req.body.title,
           summary: req.body.summary,
           body: req.body.content,
+          // date: new Date()
         },
       }
     );
+
+  res.redirect('/posts');
+});
+
+router.post('/posts/:id/delete', async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection('posts')
+    .deleteOne({ _id: postId });
   res.redirect('/posts');
 });
 
