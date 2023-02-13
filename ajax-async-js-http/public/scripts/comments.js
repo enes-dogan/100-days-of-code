3030233587938b0aea401ee0e89a09b607bdfb81
Enes-Dogan <enesdogan210@gmail.com>
2023-02-13 23:51:31 +0300

    ajax

--- ajax-async-js-http/public/scripts/comments.js
+++ ajax-async-js-http/public/scripts/comments.js
@@ -1,8 +1,16 @@
 const loadCommentsButtonElement = document.getElementById('load-comments-btn');
 
-function fetchCommentsForPost(event) {
+async function fetchCommentsForPost(event) {
   const postId = loadCommentsButtonElement.dataset.postid;
-  fetch(`/posts/${postId}/comments`);
+  const response = await fetch(`/posts/${postId}/comments`);
+  const responseData = await response.json();
+
+  const commentsSectionElement = document.getElementById('comments');
+  commentsSectionElement.innerHTML = responseData;
+
+  console.log(responseData);
+  console.log(responseData[1]);
+
 }
 
 loadCommentsButtonElement.addEventListener('click', fetchCommentsForPost);
