da19c374a2201be0d83c1c165deef36865bd001a
Enes-Dogan <enesdogan210@gmail.com>
2023-02-17 23:44:24 +0300

    .

--- user-authentication/app.js
+++ user-authentication/app.js
@@ -14,7 +14,7 @@ const app = express();
 const sessionStore = new MongoDBStore({
   uri: 'mongodb://localhost:27017',
   databaseName: 'auth-demo',
-  collection: 'sessions'
+  collection: 'sessions',
 });
 
 app.set('view engine', 'ejs');
@@ -23,18 +23,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret: 'super-secret',
-  resave: false,
-  saveUninitialized: false,
-  store: sessionStore
-}));
+app.use(
+  session({
+    secret: 'super-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
+app.use(function (error, req, res, next) {
   res.render('500');
-})
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
