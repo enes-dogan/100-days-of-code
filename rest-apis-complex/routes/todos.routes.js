dd2514411bccee659dec312e676f57048caf7000
Enes-Dogan <enesdogan210@gmail.com>
2023-05-08 18:37:04 +0300

    creating backend that stores, updates, delete and sends list of todos by API endpoints regardless of who sends it

---
+++ rest-apis-complex/routes/todos.routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const todosController = require('../controllers/todos.controller');
+
+router.get('/', todosController.getAllTodos);
+
+router.post('/', todosController.addTodo);
+
+router.patch('/:id', todosController.updateTodo);
+
+router.delete('/:id', todosController.deleteTodo);
+
+module.exports = router;
